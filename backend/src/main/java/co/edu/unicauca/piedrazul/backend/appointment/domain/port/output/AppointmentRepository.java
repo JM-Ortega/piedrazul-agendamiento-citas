3a2e8aef2744bd54dc33a552b4952a8144397882
anthonyortega22 <anthonyortega@unicauca.edu.co>
2026-03-21 12:08:43 -0500

    🛡️ types [appointment]: definir puertos del dominio

--- backend/src/main/java/co/edu/unicauca/piedrazul/backend/appointment/domain/port/output/AppointmentRepository.java
+++ backend/src/main/java/co/edu/unicauca/piedrazul/backend/appointment/domain/port/output/AppointmentRepository.java
@@ -1,4 +1,19 @@
 package co.edu.unicauca.piedrazul.backend.appointment.domain.port.output;
 
+import co.edu.unicauca.piedrazul.backend.appointment.domain.model.Appointment;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
 public interface AppointmentRepository {
+
+    //Metodos que solo hablan en terminos del dominio
+    Appointment save(Appointment appointment);
+    void deleteById(UUID idCita);
+    List<Appointment> findByDoctorId(UUID idDoctor);
+    List<Appointment> findByDate(LocalDate date);
+    List<Appointment> findByDoctorIdAndDate(UUID idDoctor, LocalDate date);
+
+
 }
