8e3a29f786c714d04894a6e0056bed42a2e7b370
MaryuriFernandez <maryurifernandez437@gmail.com>
2026-03-20 23:52:17 -0500

    💄ui: se agregan iconos nuevos a usar

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,12 +1,16 @@
 import { provideHttpClient } from '@angular/common/http';
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { LucideAngularModule, Search, CheckCircle, User, Stethoscope, UserSearch } from 'lucide-angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+    importProvidersFrom(
+      LucideAngularModule.pick({ Search, CheckCircle, User, Stethoscope, UserSearch })
+    )
   ],
 };
