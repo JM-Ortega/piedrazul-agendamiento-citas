0a8ad2baf314553f9f9b0344d54c431f012138cf
Juan Ortega <jmo.narvaez@gmail.com>
2026-03-22 16:57:40 -0500

    ✨ feat [appointment]: integrar consulta de paciente externo

--- backend/src/main/java/co/edu/unicauca/piedrazul/backend/appointment/infrastructure/integration/PatientConsultPortImpl.java
+++ backend/src/main/java/co/edu/unicauca/piedrazul/backend/appointment/infrastructure/integration/PatientConsultPortImpl.java
@@ -1,14 +1,11 @@
 package co.edu.unicauca.piedrazul.backend.appointment.infrastructure.integration;
 
-import co.edu.unicauca.piedrazul.backend.appointment.domain.model.DocumentType;
-import co.edu.unicauca.piedrazul.backend.appointment.domain.model.Gender;
 import co.edu.unicauca.piedrazul.backend.appointment.domain.model.PatientInfo;
 import co.edu.unicauca.piedrazul.backend.appointment.domain.port.output.PatientConsultPort;
-//import co.edu.unicauca.piedrazul.backend.appointment.infrastructure.mappers.PatientInfoMapper;
+import co.edu.unicauca.piedrazul.backend.appointment.infrastructure.mappers.PatientInfoMapper;
 import co.edu.unicauca.piedrazul.backend.patients.PatientModuleApi;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
 import java.util.UUID;
 
 @Component
@@ -22,9 +19,8 @@ public class PatientConsultPortImpl implements PatientConsultPort {
 
     @Override
     public PatientInfo findById(UUID idPatient) {
-        LocalDate fecha = LocalDate.of(2026, 3, 22);
-        PatientInfo p = PatientInfo.of(DocumentType.CEDULA, "123456789", "John Doe", "555-1234", "123123", Gender.FEMALE,
-                fecha, "asda", "asda");
-        return p;
+        return patientModuleApi.findById(idPatient)
+                .map(PatientInfoMapper::toPatientInfo)
+                .orElseThrow(() -> new RuntimeException("Patient not found: " + idPatient));
     }
 }
