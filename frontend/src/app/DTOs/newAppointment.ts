da8b690c836e7067664d6d2c5d2591819630e85a
MaryuriFernandez <maryurifernandez437@gmail.com>
2026-03-22 17:43:03 -0500

    ➕add: añadir datos del paciente en NewAppointemt
    se agregan más datos de la información del paciente, ya que si no se encuentra registrado, se deberían enviar al mismo tiempo en que se agenda. No se agrega el paciente si no se encuentra al inicio.

--- frontend/src/app/DTOs/newAppointment.ts
+++ frontend/src/app/DTOs/newAppointment.ts
@@ -1,6 +1,17 @@
 export interface NewAppointment {
-  patientId: string;
   doctorId: string;
+  specialty: string;
   date: string;
   time: string;
+  schedulingOrigin: 'AUTONOMO' | 'MANUAL';
+  patientId: string;
+  documentType: string;
+  documentNumber: string;
+  firstName: string;
+  lastName: string;
+  phone: string;
+  gender: 'MASCULINO' | 'FEMENINO' | 'OTRO';
+  birthDate: string;
+  email?: string;
+  guardianPhone?: string;
 }
