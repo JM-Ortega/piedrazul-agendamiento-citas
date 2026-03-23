61b84c722ddd9cb58871ce2d3d03b4f8f2b2102b
Laura Isabel Molano Bermúdez <lauraisamolano@unicauca.edu.co>
2026-03-23 11:39:03 -0500

    🐛 fix: solución de conflictos

--- backend/src/main/java/co/edu/unicauca/piedrazul/backend/appointment/model/models/enumSchedulingOrigin.java
+++
@@ -1,6 +0,0 @@
-package co.edu.unicauca.piedrazul.backend.appointment.model.models;
-
-public enum enumSchedulingOrigin {
-    WHATSAPP,
-    WEB
-}
--- backend/src/main/java/co/edu/unicauca/piedrazul/backend/appointment/model/models/enumSpecialty.java
+++
@@ -1,8 +0,0 @@
-package co.edu.unicauca.piedrazul.backend.appointment.model.models;
-
-public enum enumSpecialty {
-    MEDICO_GENERAL,
-    FISIOTERAPIA,
-    TERAPIA_NEURAL,
-    QUIROPRAXIA
-}
