29a2ad5e2b3b360421ac82d35e83212ae7c8521d
Laura Isabel Molano Bermúdez <lauraisamolano@unicauca.edu.co>
2026-03-23 23:37:23 -0500

    🔥 remove (appointment): puerto hacia patient duplicado

--- backend/src/main/java/co/edu/unicauca/piedrazul/backend/appointment/aplication/ScheduleManualAppointmentUseCaseImpl.java
+++ backend/src/main/java/co/edu/unicauca/piedrazul/backend/appointment/aplication/ScheduleManualAppointmentUseCaseImpl.java
@@ -5,7 +5,6 @@ import co.edu.unicauca.piedrazul.backend.appointment.domain.port.input.ScheduleM
 import co.edu.unicauca.piedrazul.backend.appointment.domain.port.output.AppointmentRepository;
 import co.edu.unicauca.piedrazul.backend.appointment.domain.port.output.DoctorConfigConsultPort;
 import co.edu.unicauca.piedrazul.backend.appointment.domain.port.output.PatientConsultPort;
-import co.edu.unicauca.piedrazul.backend.appointment.domain.port.output.PatientRegistryPort;
 import co.edu.unicauca.piedrazul.backend.appointment.domain.service.AppointmentService;
 import co.edu.unicauca.piedrazul.backend.appointment.infrastructure.api.dto.PatientRegistrationData;
 
@@ -19,47 +18,18 @@ public class ScheduleManualAppointmentUseCaseImpl implements ScheduleManualAppoi
     private final DoctorConfigConsultPort doctorConfigConsultPort;
     private final AppointmentService appointmentService;
     private final PatientConsultPort patientConsultPort;
-    private final PatientRegistryPort patientRegistryPort;
 
     public ScheduleManualAppointmentUseCaseImpl(
             AppointmentRepository appointmentRepository,
             DoctorConfigConsultPort doctorConfigConsultPort,
             AppointmentService appointmentService,
-            PatientConsultPort patientConsultPort,
-            PatientRegistryPort patientRegistryPort) {
+            PatientConsultPort patientConsultPort) {
         this.appointmentRepository   = appointmentRepository;
         this.doctorConfigConsultPort = doctorConfigConsultPort;
         this.appointmentService      = appointmentService;
         this.patientConsultPort = patientConsultPort;
-        this.patientRegistryPort = patientRegistryPort;
     }
 
-
-    /*
-    // Agendador crea la cita manualmente
-    @Override
-    public Appointment scheduleManual(PatientInfo patientInfo, UUID idDoctor, Specialty specialty, LocalDate date, AppointmentTime startTime) {
-        // 1. Obtiene la configuración del médico a través del puerto de salida
-        int intervalMinutes = doctorConfigConsultPort
-                .getIntervalMinutesByDoctor(idDoctor);
-        String doctorName = doctorConfigConsultPort.getDoctorName(idDoctor);
-
-        // 2. Obtiene las citas existentes del médico ese día a través del puerto de salida
-        List<Appointment> existingAppointments = appointmentRepository
-                .findByDoctorIdAndDate(idDoctor, date);
-
-        // 3. Delega la lógica de negocio al servicio de dominio
-        Appointment appointment = appointmentService.scheduleManual(
-                doctorName, patientInfo, idDoctor, specialty,
-                date, startTime, intervalMinutes, existingAppointments
-        );
-
-        // 4. Persiste a través del puerto de salida
-        appointmentRepository.save(appointment);
-
-        return appointment;
-    }
-*/
     // Agendador crea la cita manualmente
     @Override
     public Appointment scheduleManual(PatientInfo patientInfo,
@@ -89,7 +59,7 @@ public class ScheduleManualAppointmentUseCaseImpl implements ScheduleManualAppoi
             // No existe
             finalPatientInfo = patientInfo;
 
-            idPatient = patientRegistryPort.createPatient(
+            idPatient = patientConsultPort.createPatient(
                     new PatientRegistrationData(
                             finalPatientInfo.getDocumentType(),
                             finalPatientInfo.getDocumentNumber(),
