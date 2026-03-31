4a9029cdf6286abd67658d62f0d72e064dadcece
Laura Isabel Molano Bermúdez <lauraisamolano@unicauca.edu.co>
2026-03-31 12:31:23 -0500

    ➕ add (appointment): dependencia

--- backend/src/main/java/co/edu/unicauca/piedrazul/backend/appointment/package-info.java
+++ backend/src/main/java/co/edu/unicauca/piedrazul/backend/appointment/package-info.java
@@ -1,4 +1,4 @@
-@ApplicationModule(allowedDependencies = {"patients::api", "doctors :: api", "doctors", "patients"})
+@ApplicationModule(allowedDependencies = {"patients::api", "doctors :: api", "doctors", "patients", "shared"})
 package co.edu.unicauca.piedrazul.backend.appointment;
 
 import org.springframework.modulith.ApplicationModule;
