7a0d20dadd8509be2a1f4dce7d08b9a8966741f7
Laura Isabel Molano Bermúdez <lauraisamolano@unicauca.edu.co>
2026-04-01 13:40:50 -0500

    remove (appointment): 🔥 eliminar capa aplication mal estructurada
    remover implementaciones y pruebas con naming incorrecto para dar paso a la nueva estructura application

--- backend/src/main/java/co/edu/unicauca/piedrazul/backend/appointment/aplication/AppointmentExternalServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package co.edu.unicauca.piedrazul.backend.appointment.aplication;
-
-import co.edu.unicauca.piedrazul.backend.appointment.AppointmentExternalService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class AppointmentExternalServiceImpl implements AppointmentExternalService {
-}
