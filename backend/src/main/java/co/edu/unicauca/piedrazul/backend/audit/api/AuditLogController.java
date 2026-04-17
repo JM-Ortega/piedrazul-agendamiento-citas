b7b65a85087321b9434f0e28d87e8e29a6142f6f
Laura Isabel Molano Bermúdez <lauraisamolano@unicauca.edu.co>
2026-04-17 07:23:26 -0500

    ✨ feat(audit): exponer api e integracion externa de auditoria
    crear controlador de auditoria y servicio externo
    para permitir consulta e integracion con otros modulos

---
+++ backend/src/main/java/co/edu/unicauca/piedrazul/backend/audit/api/AuditLogController.java
@@ -0,0 +1,4 @@
+package co.edu.unicauca.piedrazul.backend.audit.api;
+
+public class AuditLogController {
+}
---
+++ backend/src/main/java/co/edu/unicauca/piedrazul/backend/audit/package-info.java
@@ -0,0 +1,4 @@
+@ApplicationModule
+package co.edu.unicauca.piedrazul.backend.audit;
+
+import org.springframework.modulith.ApplicationModule;
