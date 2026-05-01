5065798ef35c45703c68717035bf2b53a7570ba3
Juan M. Ortega <jmo.narvaez@gmail.com>
2026-05-01 04:08:26 -0500

    ✨ feat [user]: exponer consulta interna de agendadores keycloak por rol

---
+++ backend/src/main/java/co/edu/unicauca/piedrazul/backend/config/security/package-info.java
@@ -0,0 +1,4 @@
+@NamedInterface("security")
+package co.edu.unicauca.piedrazul.backend.config.security;
+
+import org.springframework.modulith.NamedInterface;
---
+++ backend/src/main/java/co/edu/unicauca/piedrazul/backend/user/package-info.java
@@ -0,0 +1,4 @@
+@ApplicationModule(allowedDependencies = {"shared::auth", "config::security"})
+package co.edu.unicauca.piedrazul.backend.user;
+
+import org.springframework.modulith.ApplicationModule;
