904430268dea335c673287a0846ace5da5c3897d
MaryuriFernandez <maryurifernandez437@gmail.com>
2026-05-22 11:23:39 -0500

    💄ui: actualización de formato de especialidades

--- frontend/src/app/features/appointment/components/seleccion-horario/booking-schedule-selector.component.ts
+++ frontend/src/app/features/appointment/components/seleccion-horario/booking-schedule-selector.component.ts
@@ -9,6 +9,7 @@ import { LucideAngularModule, UserSearch } from 'lucide-angular';
 import { BookingStateService } from '../../services/booking-state.service';
 import { CalendarService } from '../../services/calendar.service';
 import { NuevaCitaService } from '../../services/nuevaCita.service';
+import {SpecialtyPipe} from "../seleccion-especialidad/specialtyPipe";
 
 /**
  * Permite al usuario elegir una fecha y hora
@@ -28,6 +29,7 @@ import { NuevaCitaService } from '../../services/nuevaCita.service';
     MatInputModule,
     MatFormFieldModule,
     MatNativeDateModule,
+    SpecialtyPipe,
   ],
   templateUrl: './booking-schedule-selector.component.html',
 })
