6733158e2cc843b04348950a4d5c86110f772c52
Laura Isabel Molano Bermúdez <lauraisamolano@unicauca.edu.co>
2026-05-25 00:16:39 -0500

    ♻️ refactor(jackson): mover normalizadores al paquete principal

---
+++ backend/src/main/java/co/edu/unicauca/piedrazul/backend/jackson/NormalizeName.java
@@ -0,0 +1,16 @@
+package co.edu.unicauca.piedrazul.backend.jackson;
+
+import com.fasterxml.jackson.annotation.JacksonAnnotationsInside;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.RECORD_COMPONENT})
+@Retention(RetentionPolicy.RUNTIME)
+@JacksonAnnotationsInside
+@JsonDeserialize(using = NameSanitizerDeserializer.class)
+public @interface NormalizeName {
+}
---
+++ backend/src/main/java/co/edu/unicauca/piedrazul/backend/jackson/package-info.java
@@ -0,0 +1,4 @@
+@NamedInterface("normalization")
+package co.edu.unicauca.piedrazul.backend.jackson;
+
+import org.springframework.modulith.NamedInterface;
