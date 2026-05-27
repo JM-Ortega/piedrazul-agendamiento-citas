4b845c794d6c5155e07fa9528f90b9fe9921b200
Laura Isabel Molano Bermúdez <lauraisamolano@unicauca.edu.co>
2026-05-27 14:52:58 -0500

    ✨ feat(doctors): exponer endpoint de horarios disponibles

--- backend/src/main/java/co/edu/unicauca/piedrazul/backend/doctors/api/SheduleController.java
+++ backend/src/main/java/co/edu/unicauca/piedrazul/backend/doctors/api/SheduleController.java
@@ -122,10 +122,11 @@ public class SheduleController {
         List<LocalTime> availableIntervals = scheduleService.getAvailableIntervalsByWorkday(doctor, workday);
 
         AvailableIntervalsResponse response = new AvailableIntervalsResponse(
-            workday.toString(),
-            availableIntervals
+                workday.toString(),
+                availableIntervals
         );
 
         return ResponseEntity.ok(response);
     }
+
 }
