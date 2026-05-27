c1d4e8c36198f754d26888c69e388f970ec70f82
Laura Isabel Molano Bermúdez <lauraisamolano@unicauca.edu.co>
2026-05-26 19:08:02 -0500

    🔥 remove: eliminar módulo admin
    Eliminar el módulo admin tras haber trasladado sus responsabilidades al módulo user para centralizar la gestión de usuarios del sistema.

--- backend/src/main/java/co/edu/unicauca/piedrazul/backend/admin/api/dto/DoctorUserDataResponse.java
+++
@@ -1,8 +0,0 @@
-package co.edu.unicauca.piedrazul.backend.admin.api.dto;
-
-public record DoctorUserDataResponse(
-        String specialty,
-        String startTime,
-        String endTime,
-        int interval
-) {}
