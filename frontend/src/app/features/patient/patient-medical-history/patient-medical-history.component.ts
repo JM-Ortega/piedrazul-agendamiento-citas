0c4999b17c5e466fa9ed34acfc1acc72b424c356
MaryuriFernandez <maryurifernandez437@gmail.com>
2026-05-29 12:31:53 -0500

    ✨feat: funcionalidad de historial clínico para paciente

--- frontend/src/app/features/patient/patient-medical-history/patient-medical-history.component.ts
+++ frontend/src/app/features/patient/patient-medical-history/patient-medical-history.component.ts
@@ -1,20 +1,40 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { FormatoPipe } from '../../../shared/pipes/formatoPipe';
 import {
-  LucideAngularModule, FolderOpen, Calendar,
+  LucideAngularModule, FolderOpen, Calendar, ArrowLeft, ClipboardList,
 } from 'lucide-angular';
+import {MedicalRecord} from '../../../shared/models/dtos/medicalRecord.dto';
+import {AppService} from '../../../core/services/app.service';
+import { PatientService } from '../../../core/services/patient.service';
 
 @Component({
   selector: 'app-patient-medical-history',
   templateUrl: './patient-medical-history.component.html',
   standalone: true,
-  imports: [RouterLink, LucideAngularModule, FormatoPipe],
+  imports: [RouterLink, LucideAngularModule],
 })
 export class PatientMedicalHistoryComponent implements OnInit {
-    readonly FolderOpen = FolderOpen;
-    readonly Calendar = Calendar;
+  protected appService = inject(AppService);
+  protected patientService = inject(PatientService);
 
-    ngOnInit(): void {
-    }
+  readonly FolderOpen = FolderOpen;
+  readonly Calendar = Calendar;
+  readonly ArrowLeft = ArrowLeft;
+  readonly ClipboardList = ClipboardList;
+
+  records = signal<MedicalRecord[]>([]);
+  error = signal<string | null>(null);
+
+  ngOnInit(): void {
+    this.patientService.getMe().subscribe({
+      next: (patient) => {
+        this.patientService.loadMyMedicalRecords(patient.id);
+        this.records = this.patientService.medicalRecords;
+        this.error = this.patientService.error;
+      },
+      error: () => {
+        this.error.set('No se pudo obtener la información del paciente.');
+      }
+    });
+  }
 }
