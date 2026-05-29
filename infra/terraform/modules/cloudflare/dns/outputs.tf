4bc3f54b63b5e30bfcc40d39c4be064010d1190e
Juan M. Ortega <jmo.narvaez@gmail.com>
2026-05-29 16:27:52 -0500

    🐛 fix [cloudflare]: corregir fqdn expuestos en outputs DNS

--- infra/terraform/modules/cloudflare/dns/outputs.tf
+++ infra/terraform/modules/cloudflare/dns/outputs.tf
@@ -1,14 +1,14 @@
 output "frontend_fqdn" {
   description = "FQDN del frontend"
-  value       = "${var.project}.${var.base_domain}"
+  value       = var.base_domain
 }
 
 output "api_fqdn" {
   description = "FQDN del backend API"
-  value       = "api.${var.project}.${var.base_domain}"
+  value       = "api.${var.base_domain}"
 }
 
 output "auth_fqdn" {
   description = "FQDN del servicio de autenticación"
-  value       = "auth.${var.project}.${var.base_domain}"
+  value       = "auth.${var.base_domain}"
 }
