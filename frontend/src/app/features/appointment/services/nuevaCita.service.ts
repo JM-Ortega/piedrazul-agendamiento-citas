9be3993a6d4dd3d7e349af07904ae4bf2378b643
MaryuriFernandez <maryurifernandez437@gmail.com>
2026-05-28 22:32:36 -0500

    ♻️refactor: nuevos endpoints actualizados en backend

--- frontend/src/app/features/appointment/services/nuevaCita.service.ts
+++ frontend/src/app/features/appointment/services/nuevaCita.service.ts
@@ -28,14 +28,18 @@ export class NuevaCitaService {
     );
   }
 
-  getSpecialtiesWithDoctor(): Observable<SpecialtyDoctor[]> {
-    return this.http.get<SpecialtyDoctor[]>(
-      `${this.apiUrl}/appointments/specialties-with-doctor`,
-    );
+  getSpecialtiesWithDoctor(patientId: string | null): Observable<SpecialtyDoctor[]> {
+    const url = patientId
+      ? `${this.apiUrl}/appointments/specialties-with-doctor/${patientId}`
+      : `${this.apiUrl}/appointments/specialties-with-doctor`;
+    return this.http.get<SpecialtyDoctor[]>(url);
   }
 
-  getSpecialties(patientId: string): Observable<string[]> {
-    return this.http.get<string[]>(`${this.apiUrl}/doctor/doctors/patients/${patientId}/specialities`);
+  getSpecialties(patientId: string | null): Observable<string[]> {
+    const url = patientId
+    ? `${this.apiUrl}/doctor/doctors/patients/specialities/${patientId}`
+    : `${this.apiUrl}/doctor/doctors/specialities`;
+    return this.http.get<string[]>(url);
   }
 
   getDoctorsBySpecialty(specialty: string): Observable<SpecialtyDoctor[]> {
