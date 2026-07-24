ae4086daf660bb140a28c18d84167214103a4c47
MaryuriFernandez <maryurifernandez437@gmail.com>
2026-07-24 18:57:20 -0500

    🔥remove: eliminar botón innecesario

--- frontend/src/app/features/appointment/pages/agendador-agendar/new-appointment-scheduler.component.ts
+++ frontend/src/app/features/appointment/pages/agendador-agendar/new-appointment-scheduler.component.ts
@@ -1,13 +1,11 @@
 import { Component, inject, ChangeDetectionStrategy } from '@angular/core';
 import { Router } from '@angular/router';
 import { AppointmentBookingComponent } from '../../components/orquestador-agendamiento/appointment-booking.component';
-import { LucideArrowLeft } from '@lucide/angular';
-import { ButtonComponent } from '../../../../design-system/atoms/button.component';
 
 @Component({
   selector: 'app-new-appointment-scheduler',
   standalone: true,
-  imports: [LucideArrowLeft, AppointmentBookingComponent, ButtonComponent],
+  imports: [AppointmentBookingComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './new-appointment-scheduler.component.html',
 })
