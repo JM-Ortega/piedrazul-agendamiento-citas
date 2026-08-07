b0a2b29764cda04c918cfdedf27906e82f70a137
Laura Molano <lauraisamolano@unicauca.edu.co>
2026-08-07 18:15:09 -0500

    ✨ feat(clinical-history): recuperar consulta de historial clínico

--- backend/src/main/java/co/edu/unicauca/piedrazul/backend/clinicalHistory/ClinicalHistoryExternalService.java
+++ backend/src/main/java/co/edu/unicauca/piedrazul/backend/clinicalHistory/ClinicalHistoryExternalService.java
@@ -1,6 +1,10 @@
 package co.edu.unicauca.piedrazul.backend.clinicalHistory;
 
 import co.edu.unicauca.piedrazul.backend.clinicalHistory.api.dto.internal.ClinicalHistoryRequest;
+import co.edu.unicauca.piedrazul.backend.clinicalHistory.api.dto.output.ClinicalHistoryResponse;
+
+import java.util.List;
+import java.util.UUID;
 
 //Define qué puede hacer el módulo sin decir cómo lo hace
 public interface ClinicalHistoryExternalService {
@@ -10,5 +14,7 @@ public interface ClinicalHistoryExternalService {
     //Solo necesita saber qué puede pedirle
 
     void registerClinicalHistory(ClinicalHistoryRequest request);
+
+    List<ClinicalHistoryResponse> getHistoryByPatient(UUID idPatient);
 }
 
