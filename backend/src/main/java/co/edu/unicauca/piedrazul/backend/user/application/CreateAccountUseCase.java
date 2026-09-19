3d871550e0ce1a9acf46489da7860de1e2d31078
Laura Molano <lauraisamolano@unicauca.edu.co>
2026-09-19 12:12:25 -0500

    ✨ feat (user): hacer obligatorio el correo al crear usuarios

--- backend/src/main/java/co/edu/unicauca/piedrazul/backend/user/application/CreateAccountUseCase.java
+++ backend/src/main/java/co/edu/unicauca/piedrazul/backend/user/application/CreateAccountUseCase.java
@@ -167,7 +167,9 @@ public class CreateAccountUseCase implements UserProvisioningApi {
                 Set.of(Role.SCHEDULER),
                 Set.of(Role.DOCTOR, Role.SCHEDULER),
                 Set.of(Role.DOCTOR, Role.PATIENT),
-                Set.of(Role.DOCTOR, Role.SCHEDULER, Role.PATIENT)
+                Set.of(Role.DOCTOR, Role.SCHEDULER, Role.PATIENT),
+                Set.of(Role.DOCTOR, Role.SCHEDULER, Role.ADMIN),
+                Set.of(Role.DOCTOR, Role.SCHEDULER, Role.ADMIN, Role.PATIENT)
         );
 
         if (!validCombinations.contains(roleSet)) {
