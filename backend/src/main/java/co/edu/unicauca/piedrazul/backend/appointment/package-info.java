22332425b6b8a2ee2169c30ffb64b29dce5e27ce
Laura Molano <lauraisamolano@unicauca.edu.co>
2026-09-19 16:31:36 -0500

    ♻️ refactor: eliminar dependencias sin uso

--- backend/src/main/java/co/edu/unicauca/piedrazul/backend/appointment/package-info.java
+++ backend/src/main/java/co/edu/unicauca/piedrazul/backend/appointment/package-info.java
@@ -1,4 +1,4 @@
-@ApplicationModule(allowedDependencies = {"patients::api", "doctors::api", "doctors", "patients", "shared", "shared::events", "doctors::output-dtos", "config::validation", "jackson::sanitization", "user", "shared::audit-events", "shared::enums", "jackson::validation", "clinicalHistory::", "doctors :: internal-dto", "jackson :: normalization"})
+@ApplicationModule(allowedDependencies = {"patients::api", "doctors::api", "doctors", "patients", "shared", "doctors::output-dtos", "jackson::sanitization", "user", "shared::audit-events", "shared::enums", "jackson::validation", "doctors :: internal-dto", "jackson :: normalization"})
 package co.edu.unicauca.piedrazul.backend.appointment;
 
 import org.springframework.modulith.ApplicationModule;
